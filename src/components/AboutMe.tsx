852351c0ac2fc67b4653662b9177810e4eb75832
ssmilesquare <ssmilesquare@gmail.com>
2021-08-29 17:28:15 +0700

    Update_AboutMe

---
+++ src/components/AboutMe.tsx
@@ -0,0 +1,11 @@
+const AboutMe = () => {
+    return(
+        <div className='my-5'>
+            <div className='text-center space-y-3'>
+                <p className='text-2xl font-semibold'>About me</p>
+                <p className='text-xl'>Kanyanatthawee Siri 630610715</p>
+            </div>
+        </div>
+    )
+}
+export default AboutMe
